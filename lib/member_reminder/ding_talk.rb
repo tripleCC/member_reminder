34a70c1c6651637462b9c0e12be5c8153676b8e1
tripleCC <triplec.linux@gmail.com>
2018-08-22 10:51:19 +0800

    feat: 转换item -》 array

--- lib/member_reminder/ding_talk.rb
+++ lib/member_reminder/ding_talk.rb
@@ -1,6 +1,7 @@
 module MemberReminder
   module DingTalk
     def post(token, content, mobiles = [])
+      mobiles = Array(mobiles)
       token ||= bc0c9e48cda8f2b4c1d37a033cdbc0cd35e42fbf89b789732a3370c0b2bc314e
       curl = %Q[
             curl 'https://oapi.dingtalk.com/robot/send?access_token=#{token}' \
